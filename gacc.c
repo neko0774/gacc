2fef3654410528b6712e1d1dbb1abc8e36316160
neko0774 <neko0774@gmail.com>
2021-10-08 23:08:22 +0200

    implemented tokenizer

--- gacc.c
+++ gacc.c
@@ -1,5 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <string.h>
+
+
+typedef enum{
+	TK_RESERVED,
+	TK_NUM,
+	TK_EOF,
+} TokenKind,
+
+typedef struct  Token Token;
+
+struct Token{
+	TokenKind Kind;
+	Token *next;
+	int val;
+	cahr *dtr;
+};
+
+
+Token *token;
+
+void error(char *fmt, ...){
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+bool consum(char op){
+	if (token->kind != TK_RESERVED | token->str[0] != op)
+		return false;
+	token = token -> next;
+	return true
+}
+
+void expect_number(){
+	if (token->kind != TK_NUM)
+		error("not a number");
+	int val = token->val;
+	token = token -> next;
+	return val;
+}
+
+bool at_eof() {
+	return token->kind = TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // skiop blank
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("cannot tokenize");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
 	    if (argc != 2) {
@@ -7,28 +95,26 @@ int main(int argc, char **argv) {
 	        return 1;
 	    }
 
-	    char *p = argv[1];
 
+		token = tokenize(argv[1])
+		
+		//begining of the assembly
 	    printf(".intel_syntax noprefix\n");
 	    printf(".globl main\n");
 	    printf("main:\n");
-	    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+	    printf("  mov rax, %ld\n", expect_number());
 
+		  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
+      continue;
+    }
 
-	    while (*p){
-		if (*p == '+'){
-		p++;
-		printf("  add rax, %ld\n", strtol(p, &p, 10));
-		}
-		else if(*p == '-'){
-		p++;
-		printf("  sub rax, %ld\n", strtol(p, &p, 10));
-		}
-		else {
-		fprintf(stderr, "unexpected letter: '%c'\n", *p);
-		return 1;
-		}
-	    }
-	    printf("  ret\n");
-            return 0;
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
+  }
+
+  printf("  ret\n");
+  return 0;
+          
 }
